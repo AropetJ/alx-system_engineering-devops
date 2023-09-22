8097664a008122bd506b85a9e1764e1cde1cf5cc
AropetJ <aropetjoel32@gmail.com>
2023-09-22 19:09:14 +0300

    Completed task 3

---
+++ 0x0A-configuration_management/2-execute_a_command.pp
@@ -0,0 +1,13 @@
+# A puppet manifest that kills a process named killmenow.
+
+exec { 'killmenow':
+  command     => 'pkill -f "killmenow"',
+  path        => '/usr/bin:/usr/sbin:/bin:/sbin',
+  refreshonly => true,
+}
+
+notify { 'process_killed':
+  message => 'The "killmenow" process has been terminated.',
+  before  => Exec['your_command_here'],
+}
+
