47f2f316a3af47f49f9fba7264c8d363002f3879
AropetJ <aropetjoel32@gmail.com>
2023-09-22 22:34:04 +0300

    Completed task 2

--- 0x0A-configuration_management/2-execute_a_command.pp
+++ 0x0A-configuration_management/2-execute_a_command.pp
@@ -1,20 +1,5 @@
 # A puppet manifest that kills a process named killmenow.
-
-exec { 'killmenow':
-  command     => 'pkill -f killmenow',
-  path        => ['/bin', '/usr/bin'],
-  refreshonly => true,
-}
-
-# Suppress the "executed successfully" notice
-notify { 'process_killed':
-  message => 'The process killmenow has been terminated.',
-  before  => Exec['your_command_here'],
-}
-
-# Ensure that Puppet displays only the "Finished catalog run" notice
-notify { 'finished_run':
-  message   => 'Finished catalog run',
-  subscribe => Exec['killmenow'],
+exec { 'pkill killmenow':
+  path => '/usr/sbin:/usr/bin:/bin'
 }
 
