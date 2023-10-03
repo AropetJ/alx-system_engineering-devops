fdee562395b44d226bec4a350613f8a042e82519
AropetJ <aropetjoel32@gmail.com>
2023-10-03 20:34:15 +0300

    Attempted task 2

--- 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
+++ 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
@@ -6,7 +6,7 @@ exec { 'update_system':
 # Install Nginx and set up the custom HTTP header
 package { 'nginx':
     ensure => 'installed',
-	require => Exec['update system'],
+	require => Exec['update system']
 }
 
 file { '/var/www/html/index.html':
@@ -14,13 +14,13 @@ file { '/var/www/html/index.html':
 }
 
 # Configure Nginx settings using a file resource
-file { '/etc/nginx/sites-available/default':
+file { 'redirect_me':
     command => 'sed -i "24i\	rewrite ^/redirect_me https://th3-gr00t.tk/ permanent;" /etc/nginx/sites-available/default',
 	provider => 'shell'
 }
 
 # Use Exec to append custom HTTP header
-exec { 'add_custom_header':
+exec { 'HTTP header':
     command => 'sed -i "25i\	add_header X-Served-By \$hostname;" /etc/nginx/sites-available/default',
 	provider => 'shell'
 }
@@ -28,5 +28,5 @@ exec { 'add_custom_header':
 # Ensure Nginx service is running and enable it
 service { 'nginx':
     ensure => running,
-	require => Package['nginx'],
+	require => Package['nginx']
 }
