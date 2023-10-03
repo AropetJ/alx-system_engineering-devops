1607b52780c9e7e368790b417996e0a936b23088
AropetJ <aropetjoel32@gmail.com>
2023-10-03 20:38:25 +0300

    Attempted task 2

--- 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
+++ 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
@@ -1,32 +1,32 @@
 # Update system
-exec { 'update_system':
-    command => '/usr/bin/apt-get update',
+exec { 'update system':
+        command => '/usr/bin/apt-get update',
 }
 
 # Install Nginx and set up the custom HTTP header
 package { 'nginx':
-    ensure => 'installed',
+	ensure => 'installed',
 	require => Exec['update system']
 }
 
-file { '/var/www/html/index.html':
-    content => 'Hello World!'
+file {'/var/www/html/index.html':
+	content => 'Hello World!'
 }
 
 # Configure Nginx settings using a file resource
-file { 'redirect_me':
-    command => 'sed -i "24i\	rewrite ^/redirect_me https://th3-gr00t.tk/ permanent;" /etc/nginx/sites-available/default',
+exec {'redirect_me':
+	command => 'sed -i "24i\	rewrite ^/redirect_me https://th3-gr00t.tk/ permanent;" /etc/nginx/sites-available/default',
 	provider => 'shell'
 }
 
 # Use Exec to append custom HTTP header
-exec { 'HTTP header':
-    command => 'sed -i "25i\	add_header X-Served-By \$hostname;" /etc/nginx/sites-available/default',
+exec {'HTTP header':
+	command => 'sed -i "25i\	add_header X-Served-By \$hostname;" /etc/nginx/sites-available/default',
 	provider => 'shell'
 }
 
 # Ensure Nginx service is running and enable it
-service { 'nginx':
-    ensure => running,
+service {'nginx':
+	ensure => running,
 	require => Package['nginx']
 }
