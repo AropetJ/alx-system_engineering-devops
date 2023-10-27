2a4371ffe4e75838df44489f9dcfe0f9f8f9547a
AropetJ <aropetjoel32@gmail.com>
2023-10-27 17:20:51 +0300

    Attempted task 0

--- 0x15-api/0-gather_data_from_an_API.py
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -1,16 +1,13 @@
 #!/usr/bin/python3
 """A script that returns information about his/her TODO list progress.
 """
-
 import requests
 import sys
 
-url = "https://jsonplaceholder.typicode.com/"
-
 if __name__ == "__main__":
+    url = "https://jsonplaceholder.typicode.com/"
     employee = requests.get(url + "employees/{}".format(sys.argv[1])).json()
-    todo = requests.get(url + "todos",
-                        params={"employee Id": sys.argv[1]}).json()
+    todo = requests.get(url + "todos", params={"employee Id": sys.argv[1]}).json()
 
     info = [t.get('title') for t in todo if t.get(info) is True]
     print('Employee {} is done with tasks({}/{}):'.format(employee.get(
