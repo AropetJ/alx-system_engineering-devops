25b0ffd3690fb343db6a3be0d5796a80cd094149
AropetJ <aropetjoel32@gmail.com>
2023-11-07 14:17:02 +0300

    Attempted task 0

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -1,27 +1,17 @@
 #!/usr/bin/python3
 """Declares that queries an api"""
-
 import requests
 from sys import argv
 
 
 def number_of_subscribers(subreddit):
-    """
-    A function that queries a subreddit and
-    returns th number of subscribers
-    """
+    """Queries a subreddit and returns th number of subscribers"""
+    url = 'https://www.reddit.com/r/{}/about.json'.format(subreddit)
     agent = {
         "User-Agent": "ubuntu: 0x16.api.advanced: v1.0.0 (by /u/Aropet_Joel)"
         }
-    response = requests.get('https://www.reddit.com/r/{}/about.json'.
-                            format(subreddit), headers=agent,
-                            allow_redirects=False).json()
-
+    response = requests.get(url, headers=agent, allow_redirects=False).json()
     try:
         return response.get('data').get('subscribers')
     except requests.exceptions.HTTPError as e:
         return 0
-
-
-if __name__ == "__main__":
-    number_of_subscribers(argv[1])
