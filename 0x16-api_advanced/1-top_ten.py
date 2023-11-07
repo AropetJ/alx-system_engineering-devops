fc467dbcfbd26cc9be98c3393b0f8c65685451ac
AropetJ <aropetjoel32@gmail.com>
2023-11-07 16:37:58 +0300

    Attempted task 1

--- 0x16-api_advanced/1-top_ten.py
+++ 0x16-api_advanced/1-top_ten.py
@@ -1,21 +1,30 @@
 #!/usr/bin/python3
-"""Function to print hot posts on a given Reddit subreddit."""
+"""Declares that queries an api"""
+
 import requests
+import sys
 
 
 def top_ten(subreddit):
-    """Print the titles of the 10 hottest posts on a given subreddit."""
+    """
+    Queries the Reddit API and prints the titles of the first 10
+    hot posts listed for a given subreddit.
+    """
     url = "https://www.reddit.com/r/{}/hot/.json".format(subreddit)
     headers = {
         "User-Agent": "linux:0x16.api.advanced:v1.0.0 (by /u/bdov_)"
     }
-    params = {
+    parameters = {
         "limit": 10
     }
-    response = requests.get(url, headers=headers, params=params,
+    response = requests.get(url, headers=headers, params=parameters,
                             allow_redirects=False)
     if response.status_code == 404:
         print("None")
         return
     results = response.json().get("data")
-    [print(c.get("data").get("title")) for c in results.get("children")]
+    [print(dic.get("data").get("title")) for dic in results.get("children")]
+
+
+if __name__ == "__main__":
+    top_ten(sys.argv[1])
