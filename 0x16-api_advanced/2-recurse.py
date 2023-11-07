6d16ba2f8a3d264a0a06551f9f2b46e1af0f16b0
AropetJ <aropetjoel32@gmail.com>
2023-11-08 01:29:30 +0300

    Attempted task 2

--- 0x16-api_advanced/2-recurse.py
+++ 0x16-api_advanced/2-recurse.py
@@ -1,20 +1,23 @@
 #!/usr/bin/python3
-"""
-Using reddit's API
-"""
-import requests
+"""Declares that queries an api"""
+
+from requests import get
 import sys
-after = None
 
 
 def recurse(subreddit, hot_list=[]):
-    """returning top ten post titles recursively"""
+    """
+    Queries the Reddit API and returns a list containing the titles
+    of all hot articles for a given subreddit. If no results are found
+    for the given subreddit, the function should return None.
+    """
     global after
-    user_agent = {'User-Agent': 'api_advanced-project'}
+    after = None
+    header = {'User-Agent': 'Aropet_Joel'}
     url = "https://www.reddit.com/r/{}/hot.json".format(subreddit)
     parameters = {'after': after}
-    results = requests.get(url, params=parameters, headers=user_agent,
-                           allow_redirects=False)
+    results = get(url, params=parameters, headers=header,
+                  allow_redirects=False)
 
     if results.status_code == 200:
         after_data = results.json().get("data").get("after")
