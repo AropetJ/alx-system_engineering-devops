73040405743faef992de9e85d70fc380f8e3d1d8
AropetJ <aropetjoel32@gmail.com>
2023-11-07 17:07:41 +0300

    Attempted task 3

--- 0x16-api_advanced/100-count.py
+++ 0x16-api_advanced/100-count.py
@@ -17,7 +17,7 @@ def count_words(subreddit, word_list, after="", count=[]):
     url = "https://www.reddit.com/r/{}/hot.json".format(subreddit)
     request = requests.get(url, params={'after': after},
                            allow_redirects=False,
-                           headers=headers)
+                           headers=header)
 
     if request.status_code == 200:
         data = request.json()
